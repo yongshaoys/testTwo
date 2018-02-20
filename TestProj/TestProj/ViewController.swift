842f69e7c7d883578748752ec517f11d1db92477
yongshaoys <yongshaoys@gmail.com>
2018-02-20 10:34:59 -0600

    My first project
    My first project

---
+++ TestProj/TestProj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProj
+//
+//  Created by 邵勇 on 2/20/18.
+//  Copyright © 2018 邵勇. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
